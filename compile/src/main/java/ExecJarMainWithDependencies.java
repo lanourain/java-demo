2b5def5b2460edb782188dc6fafc44c4d0115c39
lanourain <lanourain@gmail.com>
2017-08-21 14:55:43 +0800

    mod 添加备注，打jar的指令

--- compile/src/main/java/ExecJarMainWithDependencies.java
+++ compile/src/main/java/ExecJarMainWithDependencies.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 /**
  * 项目打包 jar包，通过java -jar XXX.jar 执行对应的main方法
  * 使用maven的maven-assembly-plugin插件可以打出包含依赖的jar
+ * 执行命令：mvn clean compile assembly:single
  */
 public class ExecJarMainWithDependencies {
     private static final Logger logger = LoggerFactory.getLogger(ExecJarMainWithDependencies.class);
