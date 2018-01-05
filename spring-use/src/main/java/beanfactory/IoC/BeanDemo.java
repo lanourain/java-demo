ec03a7835e06c36ff52518f156b20223f1cf85aa
lanourain <lanourain@gmail.com>
2018-01-05 13:26:20 +0800

    BeanFactory + ApplicationContext简单使用。

---
+++ spring-use/src/main/java/beanfactory/IoC/BeanDemo.java
@@ -0,0 +1,13 @@
+package beanfactory.IoC;
+
+public class BeanDemo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ spring-use/src/main/java/beanfactory/IoC/XmlBeanFactoryDemo.java
@@ -0,0 +1,19 @@
+package beanfactory.IoC;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+// 使用XmlBeanFactory
+public class XmlBeanFactoryDemo {
+
+    public static void main(String[] args) {
+
+        // 创建Resource
+        ClassPathResource res = new ClassPathResource("BeanDefinition.xml");
+        XmlBeanFactory factory = new XmlBeanFactory(res);
+        // 通过beanName获取容器中对应的Bean
+        Object beanDemo = factory.getBean("beanDemo");
+        System.out.println(beanDemo instanceof BeanDemo);
+
+    }
+}
